77d59d4c87df69fccb1ab901e889e3c2172316e0
Hoang Long <lelong.ll32@gmail.com>
2021-09-04 16:35:00 +0700

    Dependency Package

--- 14. Dependency Management/Thuchanh1/src/main/org/usefullibs/greeter/Greeter.java
+++ 14. Dependency Management/Thuchanh1/src/main/org/usefullibs/greeter/Greeter.java
@@ -1,4 +1,4 @@
-package main;
+package main.org.usefullibs.greeter;
 
 public interface Greeter {
     String greet();
--- 14. Dependency Management/Thuchanh1/src/main/org/usefullibs/greeter/impl/HelloWorld.java
+++ 14. Dependency Management/Thuchanh1/src/main/org/usefullibs/greeter/impl/HelloWorld.java
@@ -1,2 +1,10 @@
-package main;public class HelloWorld {
+package main.org.usefullibs.greeter.impl;
+
+import main.org.usefullibs.greeter.Greeter;
+
+public class HelloWorld implements Greeter {
+    @Override
+    public String greet() {
+        return "Hello world";
+    }
 }
